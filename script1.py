62e0ed8babbc8f264211aff5788758d08e159c1a
Alvaro <alvaro.a.cruz.j@gmail.com>
2024-10-22 16:48:43 -0600

    script1 done with changes

--- script1.py
+++ script1.py
@@ -4,7 +4,5 @@ url = "https://randomuser.me/api/"
 query_params = {"nat": "US"}
 
 for i in range (3):
-    print("GG")
     response = requests.get(url, params=query_params).json()
     print(response)
-    
