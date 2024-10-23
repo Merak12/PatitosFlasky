fd4444c07835dbd3f1e17ca1bb87bb99fbe9205a
Alvaro <alvaro.a.cruz.j@gmail.com>
2024-10-22 18:52:48 -0600

    script 3 and

--- script3.py
+++ script3.py
@@ -1,1 +1,26 @@
 #Escribe un script en python que consulte la API de los casos confirmados de paises y muestre el numero de casos confirmados en méxico al dia de 27 de OCTUBRE de 2021
+
+
+#Escribe un script en python que consulte la API de los casos confirmados de paises y muestre el numero de casos confirmados en méxico al dia de 27 de OCTUBRE de 2021
+import requests
+
+country = 'mexico'
+date = '2021-10-27'
+api_url = 'https://api.api-ninjas.com/v1/covid19?country={}'.format(country)
+response = requests.get(api_url, headers={'X-Api-Key': '2EaZcOXJ0BlavQNp/OCwNQ==FOldfxFGMvcWHEdp'})
+
+
+response_json = response.json()  
+
+country_data = response_json[0]  
+
+
+cases = country_data['cases']
+
+date = cases['2021-10-27']
+
+numberOfCases = date['total']
+
+print("El numero de casos confirmasdos es: ", numberOfCases)
+
+
--- script4.py
+++ script4.py
@@ -1,11 +1,19 @@
 #Escribe un script en Python que extraiga la lista de episodios de la serie "Titans" (https://en.wikipedia.org/wiki/Titans_(2018_TV_series)) y los despliegue ne la pantalla de la siguiente forma:
 
 import requests
+from bs4 import BeautifulSoup
 
-url = "https://en.wikipedia.org/wiki/Titans_(2018_TV_series)"
-response = requests.get(url)
+URL = "https://en.wikipedia.org/wiki/Titans_(2018_TV_series)"
+page = requests.get(URL)
 
-print(response.status_code)
-print(response.headers['content-type'])
-print(response.encoding)
-print(response.text)
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(class_="mw-content-ltr mw-parser-output")
+episodesTables = results.find_all("table", class_="wikitable plainrowheaders wikiepisodetable")
+
+for episodeTable in episodesTables:
+    episodeRows = episodeTable.find_all("tr", class_="vevent module-episode-list-row")
+    for episode in episodeRows:
+        episodeNumber = episode.find("th", scope="row")
+        episodeTitle = episode.find("td", class_="summary")
+        print(episodeNumber.text.strip()+" "+episodeTitle.text.strip())
+        print()
