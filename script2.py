f935eff6a1dc13526fb61a0d1daa472200ab8449
Alvaro <alvaro.a.cruz.j@gmail.com>
2024-10-22 18:02:07 -0600

    script2 done fixed

--- script2.py
+++ script2.py
@@ -1,24 +1,11 @@
 # Escribe un script en python que consulte la API de los perros y que muestre en la pantalla la url de 3 imágenes de perros
 import requests
 
-
-url_endpoint1 = "https://api.thedogapi.com/v1/breeds/1"
-response_endpoint1 = requests.get(url_endpoint1)
-
-url_endpoint2 = "https://api.thedogapi.com/v1/breeds/2"
-response_endpoint2 = requests.get(url_endpoint2)
-
-url_endpoint3 = "https://api.thedogapi.com/v1/breeds/3"
-response_endpoint3 = requests.get(url_endpoint3)
-imageID1 = response_endpoint1.json()["reference_image_id"]
-
-print(url_endpoint1)
-print(response_endpoint1.json()["reference_image_id"])
-print()
-print(url_endpoint2)
-print(response_endpoint2.json()["reference_image_id"])
-print()
-print(url_endpoint3)
-print(response_endpoint3.json()["reference_image_id"])
-print()
+for i in range(3):
+    url_endpoint = f"https://api.thedogapi.com/v1/breeds/{i+1}"
+    url_images = "https://api.thedogapi.com/v1/images/"
+    response_endpoint = requests.get(url_endpoint)
+    imageID1 = response_endpoint.json()["reference_image_id"]
+    urlSearch = requests.get(url_images + imageID1).json()
+    print(urlSearch["url"])
 
