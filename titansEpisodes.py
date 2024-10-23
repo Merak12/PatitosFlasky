818ed55fe61bbf2ee0685a16c871c695c10a8ff6
CharismaticTopHat <a0p5garcia@gmail.com>
2024-10-22 18:49:48 -0600

    Activity 2 - List of Titans episodes - Abi - Done

---
+++ titansEpisodes.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://en.wikipedia.org/wiki/Titans_(2018_TV_series)"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(class_="mw-content-ltr mw-parser-output")
+episodesTables = results.find_all("table", class_="wikitable plainrowheaders wikiepisodetable")
+
+for episodeTable in episodesTables:
+    episodeRows = episodeTable.find_all("tr", class_="vevent module-episode-list-row")
+    for episode in episodeRows:
+        episodeNumber = episode.find("th", scope="row")
+        episodeTitle = episode.find("td", class_="summary")
+        print(episodeNumber.text.strip()+" "+episodeTitle.text.strip())
+        print()
